0a0d412a22311285bd683baa70501f83b153d897
ksbains <k1bains@ucsd.edu>
2018-02-11 13:29:11 -0800

    added the expense database and changed the values to numbers

---
+++ routes/addExpense.js
@@ -0,0 +1,16 @@
+var expensesDB = require("../data/expenses.json");
+exports.addExpense = function(req, res){
+ 	
+	let expense = {	
+		name: req.query.name,
+		description: req.query.description,
+		imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Double-barred_dollar_sign.svg/2000px-Double-barred_dollar_sign.svg.png',
+		inventory: req.query.inventory
+	}
+
+	console.log("BREAK");
+	console.log("the expense to push is ", expense);
+	console.log("BREAK");
+	expensesDB.expenses.push(expense);
+	res.render("index", expensesDB);
+}
